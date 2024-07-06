92f0ec8355b3a6be14d1847e65f4c13409bacd88
hanzalahwaheed <hanzalah.w@gmail.com>
2024-07-06 15:48:35 +0530

    mobile_input done w animation

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,6 +1,6 @@
 
 const Button = ({ content }: { content: string }) => {
-  return <button className="p-4 my-4 bg-slate-200 text-zinc-400 w-full rounded text-3xl font-bold">{content}</button>;
+  return <button className="p-4 my-4 bg-slate-200 text-zinc-400 w-full rounded-lg text-3xl font-bold">{content}</button>;
 };
 
 export default Button;
